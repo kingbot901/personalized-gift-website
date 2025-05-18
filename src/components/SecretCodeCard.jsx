cb9df095bee5db185d15b4855b6588c29b5bbeac
kingbot901 <botuser9t9@gmail.com>
2025-05-18 23:12:38 +0500

    Update SecretCodeCard.jsx

--- src/components/SecretCodeCard.jsx
+++ src/components/SecretCodeCard.jsx
@@ -19,7 +19,7 @@ export default function SecretCodeCard({ onCorrect }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (code === '123456') {
+        if (code === 'Quratulain') {
             onCorrect()
         } else {
             setError("Oops! That’s not the right code.")
